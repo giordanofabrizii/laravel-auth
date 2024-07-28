3de6cff8c1e74343a467f24a55b565509d15e9a1
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-28 13:10:28 +0200

    created admin projects index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->name('admin.')->prefix('admin/')->group(function(){
+    Route::resource("/projects",AdminProjectsController::class);
+});
